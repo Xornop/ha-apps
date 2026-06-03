85ce1df37dd913028bcc35bb3fea9b357ccd201c
Xornop <32822630+Xornop@users.noreply.github.com>
2026-06-03 22:13:22 +0200

    Update run.sh

--- gluetun-addon/run.sh
+++ gluetun-addon/run.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/with-contenv bashio
 
-# Timezone
 if bashio::config.has_value 'tz'; then
     export TZ="$(bashio::config 'tz')"
 fi
 
-# VPN
 export VPN_SERVICE_PROVIDER="$(bashio::config 'vpn_provider')"
 export VPN_TYPE="$(bashio::config 'vpn_type')"
 
-# OpenVPN
 if bashio::config.has_value 'openvpn_user'; then
     export OPENVPN_USER="$(bashio::config 'openvpn_user')"
     export OPENVPN_PASSWORD="$(bashio::config 'openvpn_password')"
 fi
 
-# Wireguard
 if bashio::config.has_value 'wireguard_private_key'; then
     export WIREGUARD_PRIVATE_KEY="$(bashio::config 'wireguard_private_key')"
     export WIREGUARD_ADDRESSES="$(bashio::config 'wireguard_addresses')"
 fi
 
-# Shadowsocks
 if bashio::config.has_value 'shadowsocks_enabled' && [ "$(bashio::config 'shadowsocks_enabled')" = "true" ]; then
     export SHADOWSOCKS_ENABLED="on"
     export SHADOWSOCKS_PORT="$(bashio::config 'shadowsocks_port' '8388')"
@@ -31,7 +26,6 @@ else
     export SHADOWSOCKS_ENABLED="off"
 fi
 
-# HTTP Proxy
 if bashio::config.has_value 'http_proxy_enabled' && [ "$(bashio::config 'http_proxy_enabled')" = "true" ]; then
     export HTTPPROXY_ENABLED="on"
     export HTTPPROXY_PORT="$(bashio::config 'http_proxy_port' '8888')"
@@ -39,17 +33,14 @@ else
     export HTTPPROXY_ENABLED="off"
 fi
 
-# DNS
-if bashio::config.has_value 'dns_providers'; then
-    export DOT="on"
-    export DOT_PROVIDERS="$(bashio::config 'dns_providers')"
-fi
-
+export DOT="off"
 export UPDATER_PERIOD="24h"
 export LOG_LEVEL="info"
-export FIREWALL_ENABLED_DISABLING_IT_SHOOTS_YOU_IN_YOUR_FOOT=on
+export LOG_FORMATTER="text"
+export PPROF_ENABLED="no"
+export PPROF_BLOCK_PROFILE_RATE="0"
+export PPROF_MUTEX_PROFILE_RATE="0"
+export FIREWALL_ENABLED_DISABLING_IT_SHOOTS_YOU_IN_YOUR_FOOT="on"
 
 bashio::log.info "Starting Gluetun VPN..."
-export PPROF_ENABLED=off
-export DOT=off
 exec /usr/local/bin/gluetun
