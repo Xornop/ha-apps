6244363b647cd1aa58c75cf2e5145f5766a7ce6b
Xornop <32822630+Xornop@users.noreply.github.com>
2026-06-24 10:19:09 +0200

    Update run.sh

--- prefetcharr/run.sh
+++ prefetcharr/run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/with-contenv bashio
 
-# Read options from HA config
 MEDIA_SERVER_TYPE=$(bashio::config 'media_server_type')
 MEDIA_SERVER_URL=$(bashio::config 'media_server_url')
 MEDIA_SERVER_API_KEY=$(bashio::config 'media_server_api_key')
@@ -10,40 +9,37 @@ INTERVAL=$(bashio::config 'interval')
 PREFETCH_NUM=$(bashio::config 'prefetch_num')
 REQUEST_SEASONS=$(bashio::config 'request_seasons')
 LOG_LEVEL=$(bashio::config 'log_level')
-USERS=$(bashio::config 'users')
-LIBRARIES=$(bashio::config 'libraries')
 
-# Build TOML config
-CONFIG="
+CONFIG_FILE=/tmp/prefetcharr.toml
+
+cat > "${CONFIG_FILE}" << TOML
 interval = ${INTERVAL}
-log_level = \"${LOG_LEVEL}\"
+log_level = "${LOG_LEVEL}"
 prefetch_num = ${PREFETCH_NUM}
 request_seasons = ${REQUEST_SEASONS}
 
 [media_server]
-type = \"${MEDIA_SERVER_TYPE}\"
-url = \"${MEDIA_SERVER_URL}\"
-api_key = \"${MEDIA_SERVER_API_KEY}\"
+type = "${MEDIA_SERVER_TYPE}"
+url = "${MEDIA_SERVER_URL}"
+api_key = "${MEDIA_SERVER_API_KEY}"
 
 [sonarr]
-url = \"${SONARR_URL}\"
-api_key = \"${SONARR_API_KEY}\"
-"
+url = "${SONARR_URL}"
+api_key = "${SONARR_API_KEY}"
+TOML
 
 if bashio::config.has_value 'users'; then
-    CONFIG="${CONFIG}
-users = [$(echo "$USERS" | sed 's/,/","/g' | sed 's/^/\"/' | sed 's/$/\"/')]"
+    USERS=$(bashio::config 'users')
+    echo "users = [$(echo "$USERS" | sed 's/,/\", \"/g' | sed 's/^/\"/' | sed 's/$/\"/')]" >> "${CONFIG_FILE}"
 fi
 
 if bashio::config.has_value 'libraries'; then
-    CONFIG="${CONFIG}
-libraries = [$(echo "$LIBRARIES" | sed 's/,/","/g' | sed 's/^/\"/' | sed 's/$/\"/')]"
+    LIBRARIES=$(bashio::config 'libraries')
+    echo "libraries = [$(echo "$LIBRARIES" | sed 's/,/\", \"/g' | sed 's/^/\"/' | sed 's/$/\"/')]" >> "${CONFIG_FILE}"
 fi
 
 bashio::log.info "Starting Prefetcharr..."
 bashio::log.info "Media server: ${MEDIA_SERVER_TYPE} @ ${MEDIA_SERVER_URL}"
 bashio::log.info "Sonarr: ${SONARR_URL}"
 
-export PREFETCHARR_CONFIG="${CONFIG}"
-
-exec /usr/local/bin/prefetcharr
+exec /usr/local/bin/prefetcharr --config "${CONFIG_FILE}"
